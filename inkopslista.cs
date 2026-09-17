89115b3d19a68b74521c71d3d0e081460daf0351
Yassin22x <yassinelmarraki@outlook.com>
2026-09-17 16:17:59 +0200

    Skapar lista och lägger till varor

---
+++ inkopslista.cs
@@ -0,0 +1,12 @@
+List<string> names = new List<string>(); // Två paralella listor för respektive namn och pris. (Endast heltal på int)
+List<int> prices = new List<int>(); 
+
+names.Add("Ost");   // Lägger till Ost på plats 0
+prices.Add(15);   // Lägger till priset 15 kr på plats 0
+
+names.Add("Smör"); // Lägger till Smör på plats 1
+prices.Add(20);   // Lägger till priset 20 kr på plats 1
+
+names.Add("Salami");   // Lägger till Salami på plats 2
+prices.Add(25);   // Lägger till priset 25 kr på plats 2
+
